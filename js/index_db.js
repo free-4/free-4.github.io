3572a931770e52e18975d3a887d45cd9cbc8cbcc
free-4 <shuowork155@outlook.com>
2026-02-19 14:45:14 +0800

    更新 index_db.js

--- js/index_db.js
+++ js/index_db.js
@@ -3,39 +3,39 @@
 ========================= */
 const SITE_DATA = [
     {
-        name: "API 提供",
+        name: "1",
         icon: "api",
         items: [
-            { title: "用户API", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "user" },
-            { title: "天气API", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "weather" },
-            { title: "AI 接口", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "ai" }
+            { title: "1", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "user" },
+            { title: "2", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "weather" },
+            { title: "3", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "ai" }
         ]
     },
     {
-        name: "在线工具",
+        name: "2",
         icon: "tool",
         items: [
-            { title: "JSON 解析", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "json" },
+            { title: "1", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "json" },
             { title: "Base64 转换", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "file" },
-            { title: "图像优化", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "box" }
+            { title: "3", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "box" }
         ]
     },
     {
         name: "开发项目",
         icon: "code",
         items: [
-            { title: "云盘系统", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "cloud" },
-            { title: "开源导航", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "api" },
-            { title: "聊天系统", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "ai" }
+            { title: "云盘系统", url: "https://github.com/shuo156/Local-cloud", icon: "cloud" },
+            { title: "1", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "api" },
+            { title: "2", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "ai" }
         ]
     },
     {
         name: "资源导航",
         icon: "box",
         items: [
-            { title: "学习资料", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "study" },
-            { title: "代码模板", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "code" },
-            { title: "设计资源", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "tool" }
+            { title: "1", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "study" },
+            { title: "2", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "code" },
+            { title: "3", url: "https://www.mps.gov.cn/n2253534/n2253543/c9257095/content.html", icon: "tool" }
         ]
     }
 ];
