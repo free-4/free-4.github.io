1f76710096df8d470ecb191b32c1031b5379ec5d
free-4 <shuowork155@outlook.com>
2026-03-03 00:04:46 +0800

    更新 common.js

--- common.js
+++ common.js
@@ -1213,6 +1213,9 @@ body.pink .refresh-btn:hover {
       <a href="https://github.com/free-4/free-4.github.io/issues" class="footer-link" target="_blank">评论区</a>
     </div>
     <p>&copy; 2026 SHUOWEB.COM · 不忘初心</p><br>
+    <span id="busuanzi_container_site_pv">
+    本站总访问量<span id="busuanzi_value_site_pv"></span>次
+</span>
   `;
 
   document.body.appendChild(footer);
@@ -1268,3 +1271,5 @@ window.addEventListener('scroll', () => {
   }
   lastScroll = currentScroll;
 });
+
+script.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
