fad3e49a418118361b01a276705e9755a3a0dbad
free-4 <shuowork155@outlook.com>
2026-05-02 19:58:31 +0800

    更新 index_db.js

--- js/index_db.js
+++ js/index_db.js
@@ -9,6 +9,7 @@ const SITE_DATA = [
             { title: "历史上的今天", url: "https://shuoweb.com/page/html/today_history", icon: "history" },
             { title: "每日一言", url: "https://shuoweb.com/page/html/day_yan.html", icon: "yan" },
             { title: "世界人口时钟", url: "https://shuoweb.com/page/html/people.html", icon: "user" },
+            { title: "随机手机型号", url: "https://shuoweb.com/api/phone", icon: "phone" },
             { title: "聊天室", url: "https://shuoweb.wuaze.com/chat/", icon: "chat" }
         ]
     },
