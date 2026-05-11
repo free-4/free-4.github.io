41f0787c2730375196bef7c5280f788e743ce318
free-4 <shuowork155@outlook.com>
2026-05-11 22:42:52 +0800

    更新 db.js

--- page/study/db.js
+++ page/study/db.js
@@ -6,9 +6,9 @@ const SITE_DATA = [
         name: "学习项目（持续更新）",
         icon: "fun",
         items: [
-            { title: "英语6000词", url: "https://shuoweb.com/english/", icon: "book" },
-            { title: "生物必修二", url: "https://shuoweb.com/page/html/day_yan.html", icon: "book" },
-            { title: "化学选修二", url: "https://shuoweb.com/page/html/people.html", icon: "book" }
+            { title: "英语6000词", url: "https://shuoweb.com/english/", icon: "study" },
+            { title: "生物必修二", url: "https://shuoweb.com/page/study/biology_b2_3.html", icon: "study" },
+            { title: "化学选修二", url: "https://shuoweb.com/page/study/chemistry_x2_2.html", icon: "study" }
         ]
     }
 ];
