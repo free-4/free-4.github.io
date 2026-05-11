460a5ebf5f298c6c5b88e9d5f77e7ece67583fe1
free-4 <shuowork155@outlook.com>
2026-05-11 22:39:27 +0800

    创建 db.js

---
+++ page/study/db.js
@@ -0,0 +1,14 @@
+/* =========================
+   JSON 数据区
+========================= */
+const SITE_DATA = [
+    {
+        name: "学习项目（持续更新）",
+        icon: "fun",
+        items: [
+            { title: "英语6000词", url: "https://shuoweb.com/english/", icon: "book" },
+            { title: "生物必修二", url: "https://shuoweb.com/page/html/day_yan.html", icon: "book" },
+            { title: "化学选修二", url: "https://shuoweb.com/page/html/people.html", icon: "book" }
+        ]
+    }
+];
