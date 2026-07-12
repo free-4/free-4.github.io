c5961503c27fb6690beaa4ec4b1eda1a42f9e21c
free-4 <shuowork155@outlook.com>
2026-07-12 23:45:59 +0800

    更新 db.js

--- page/study/db.js
+++ page/study/db.js
@@ -9,7 +9,9 @@ const SITE_DATA = [
             { title: "英语6000词（离线版）", url: "https://shuoweb.com/english/", icon: "study" },
             { title: "英语6000词（在线版）", url: "https://ovo.shuoweb.com/english", icon: "study" },
             { title: "生物必修二", url: "https://shuoweb.com/page/study/biology_b2_3.html", icon: "study" },
-            { title: "化学选修二", url: "https://shuoweb.com/page/study/chemistry_x2_2.html", icon: "study" }
+            { title: "化学选修二", url: "https://shuoweb.com/page/study/chemistry_x2_2.html", icon: "study" },
+            { title: "高考志愿查询", url: "https://shuoweb.com/page/gaokao/", icon: "study" },
+            { title: "生物减数分裂", url: "https://shuoweb.com/page/study/biology/jianshu", icon: "study" }
         ]
     }
 ];
